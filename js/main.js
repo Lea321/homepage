79068f1361d9d5ed59e379985eb84237659842c3
Leonus <553344777@qq.com>
2023-07-26 20:04:05 +0800

    update

--- js/main.js
+++ js/main.js
@@ -1,61 +1,31 @@
-// 添加视差效果
-new simpleParallax(document.getElementsByClassName('banner-pic-img'), {
-  orientation: 'up',
-  scale: 1.2,
-  delay: 2,
-  transition: 'cubic-bezier(0,0,0,1)',
-  maxTransition: 50,
-  overflow: true
-});
 
-// 节流
-function throttle(fn, time = 200) {
-  let pre = 0;
-  let timeout = 0;
-  return function (...args) {
-    const now = Date.now();
-    if (now - pre > time) {
-      pre = now;
-      fn.apply(this, args);
-    } else {
-      if (timeout) {
-        clearTimeout(timeout)
-        timeout = null;
-      }
-      timeout = setTimeout(() => {
-        pre = now;
-        fn.apply(this, args);
-      }, time);
-    }
-  }
-}
-
-// 添加菜单点击事件
-document.getElementById("nav-menu").addEventListener('click', function () {
-  if (document.getElementById("body").classList.contains('show-menu')) { heoWeb.hideMenu(); } else { heoWeb.showMenu(); }
-}, false)
-
-//关闭菜单
-$('.menu-list').click(function () { heoWeb.hideMenu(); });
-
-//阻止菜单滚动
-document.querySelector('.menu-list').addEventListener('wheel', (e) => { e.preventDefault() })
-
-
-document.body.onscroll = throttle(e => {
-  let scroll = document.documentElement.scrollTop || window.pageYOffset
-  if (scroll > 500) document.getElementById('go-top').classList.add('show')
-  else document.getElementById('go-top').classList.remove('show')
-})
-
-var heoWeb = {
+var leonus = {
   //显示菜单
-  showMenu: function () {
+  showMenu() {
     document.getElementById("body").classList.add("show-menu");
   },
-
-  hideMenu: function () {
+  hideMenu() {
     document.getElementById("body").classList.remove("show-menu");
+  },
+  throttle(fn, time = 200) {
+    let pre = 0;
+    let timeout = 0;
+    return function (...args) {
+      const now = Date.now();
+      if (now - pre > time) {
+        pre = now;
+        fn.apply(this, args);
+      } else {
+        if (timeout) {
+          clearTimeout(timeout)
+          timeout = null;
+        }
+        timeout = setTimeout(() => {
+          pre = now;
+          fn.apply(this, args);
+        }, time);
+      }
+    }
   },
   //跳转到id
   scrollTo(id) {
@@ -83,81 +53,104 @@ var heoWeb = {
     }
 
     requestAnimationFrame(animation);
+  },
+  photos() {
+    fetch('https://m.leonus.cn/api/v1/memo?creatorId=1&tag=相册').then(res => res.json()).then(data => {
+      let html = '',
+        imgs = [];
+      for (let i = 0; i < data.length; i++) { if (imgs.length < 9) imgs = imgs.concat(data[i].content.match(/\!\[.*?\]\(.*?\)/g)) }
+      imgs.slice(0, 9).forEach(item => {
+        let img = item.replace(/!\[.*?\]\((.*?)\)/g, '$1'),
+          tat = item.replace(/!\[(.*?)\]\(.*?\)/g, '$1').trim();
+        html += `<img src="${img}" alt="${tat}" title="${tat}">`;
+      });
+
+      document.querySelector('.photos .banner-pic-img').innerHTML = html
+    }).catch()
+  },
+  article() {
+    fetch('https://api.leonus.cn/article').then(res => res.json()).then(data => {
+      if (!data) return
+      let dom = document.querySelector('.articles .group-items')
+      let html = ''
+      if (!dom) return
+      data.forEach((item, index) => {
+        html += `
+        <div class="group-item" title="${item.title}">
+          <a class="group-item-icon" href="${item.url}" target="_blank">
+            <img class="group-item-icon-img" src="/img/num/${index + 1}.svg" alt="product-icon" title="product-icon">
+          </a>
+          <div class="group-info-group">
+            <div class="group-info">
+              <a class="title" href="${item.url}" target="_blank">${item.title}</a>
+              <a class="discription" href="${item.url}" target="_blank">${item.desc}</a>
+            </div>
+            <div class="group-info-button">
+              <a class="linkbutton" href="${item.url}" target="_blank">阅读</a>
+            </div>
+          </div>
+        </div>`
+      });
+      dom.innerHTML = html
+    })
+  },
+  scrollToTop() {
+    const duration = 600; // in milliseconds
+    const startPosition = window.pageYOffset;
+    const distance = -window.pageYOffset;
+    let startTime = null;
+
+    function animation(currentTime) {
+      if (!startTime) {
+        startTime = currentTime;
+      }
+      const timeElapsed = currentTime - startTime;
+      const scrollY = easeInOutQuad(timeElapsed, startPosition, distance, duration);
+      window.scrollTo(0, scrollY);
+      if (timeElapsed < duration) {
+        requestAnimationFrame(animation);
+      }
+    }
+
+    function easeInOutQuad(t, b, c, d) {
+      t /= d / 2;
+      if (t < 1) return c / 2 * t * t + b;
+      t--;
+      return -c / 2 * (t * (t - 2) - 1) + b;
+    }
+
+    requestAnimationFrame(animation);
   }
 }
 
-//滚动页面动画
-function scrollToTopWithAnimation() {
-  const duration = 600; // in milliseconds
-  const startPosition = window.pageYOffset;
-  const distance = -window.pageYOffset;
-  let startTime = null;
+// 添加视差效果
+new simpleParallax(document.getElementsByClassName('banner-pic-img'), {
+  orientation: 'up',
+  scale: 1.2,
+  delay: 2,
+  transition: 'cubic-bezier(0,0,0,1)',
+  maxTransition: 50,
+  overflow: true
+});
 
-  function animation(currentTime) {
-    if (!startTime) {
-      startTime = currentTime;
-    }
-    const timeElapsed = currentTime - startTime;
-    const scrollY = easeInOutQuad(timeElapsed, startPosition, distance, duration);
-    window.scrollTo(0, scrollY);
-    if (timeElapsed < duration) {
-      requestAnimationFrame(animation);
-    }
-  }
 
-  function easeInOutQuad(t, b, c, d) {
-    t /= d / 2;
-    if (t < 1) return c / 2 * t * t + b;
-    t--;
-    return -c / 2 * (t * (t - 2) - 1) + b;
-  }
+// 添加菜单点击事件
+document.getElementById("nav-menu").addEventListener('click', () => { if (document.getElementById("body").classList.contains('show-menu')) leonus.hideMenu(); else leonus.showMenu(); }, false)
 
-  requestAnimationFrame(animation);
-}
+document.querySelector('.menu-list').onclick = () => { leonus.hideMenu(); }
+
+//阻止菜单滚动
+document.querySelector('.menu-list').addEventListener('wheel', (e) => { e.preventDefault() })
+
+
+document.body.onscroll = leonus.throttle(() => {
+  let scroll = document.documentElement.scrollTop || window.pageYOffset
+  if (scroll > 500) document.getElementById('go-top').classList.add('show')
+  else document.getElementById('go-top').classList.remove('show')
+})
 
 // 文章
-article()
-function article() {
-  fetch('https://api.leonus.cn/article').then(res => res.json()).then(data => {
-    if (!data) return
-    let dom = document.querySelector('.articles .group-items')
-    let html = ''
-    if (!dom) return
-    data.forEach((item, index) => {
-      html += `
-      <div class="group-item" title="${item.title}">
-        <a class="group-item-icon" href="${item.url}" target="_blank">
-          <img class="group-item-icon-img" src="/img/num/${index + 1}.svg" alt="product-icon" title="product-icon">
-        </a>
-        <div class="group-info-group">
-          <div class="group-info">
-            <a class="title" href="${item.url}" target="_blank">${item.title}</a>
-            <a class="discription" href="${item.url}" target="_blank">${item.desc}</a>
-          </div>
-          <div class="group-info-button">
-            <a class="linkbutton" href="${item.url}" target="_blank">阅读</a>
-          </div>
-        </div>
-      </div>`
-    });
-    dom.innerHTML = html
-  })
-}
-
+leonus.article()
 // 相册
-photos()
-function photos() {
-  fetch('https://m.leonus.cn/api/memo?creatorId=1&tag=相册').then(res => res.json()).then(data => {
-    data = data.data
-    let html = '',
-      imgs = [];
-    for (let i = 0; i < data.length; i++) { if (imgs.length < 9) imgs = imgs.concat(data[i].content.match(/\!\[.*?\]\(.*?\)/g)) }
-    imgs.slice(0, 9).forEach(item => {
-      let img = item.replace(/!\[.*?\]\((.*?)\)/g, '$1'),
-        tat = item.replace(/!\[(.*?)\]\(.*?\)/g, '$1').trim();
-      html += `<img src="${img}" alt="${tat}" title="${tat}">`;
-    });
+leonus.photos()
 
-    document.querySelector('.photos .banner-pic-img').innerHTML = html
-  }).catch()
-}
